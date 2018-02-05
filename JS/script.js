6f89f7efead6cd3d957bbe3db1f74371541a3ce2
Shantia <shantiagrimes@gmail.com>
2018-02-05 07:49:58 -0600

    Styling cards and working on board body

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,22 @@
 'use strict'
+
+var Card = {
+    template: '<div>A card component</div>'
+}
+
+Vue.component('newList')
+
+Vue.component('list', {
+    template: '#add-list-template',
+    components: {
+        'card': Card
+    }
+}) 
+
+var app = new Vue ({
+    el: '#app',
+    data: {
+        message: 'Vue is working!'
+    }
+})
+
